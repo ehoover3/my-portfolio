0eba5afa1b69da8604c67905fa35b440dcba3f07
Eric Hoover <ehoover3@yahoo.com>
2022-04-27 03:26:14 -0700

    updated accordion

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -4,10 +4,9 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 
-// images
-import deaf from "../images/deaf.png";
-import banking from "../images/banking.png";
-import instructor from "../images/instructor.png";
+// react icons
+import { FaDeaf, FaLaptopCode, FaCodeBranch } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
 
 export default function About() {
   const wrapperStyle = {
@@ -19,6 +18,8 @@ export default function About() {
   };
 
   const cardStyle = { display: "flex", justifyContent: "center", alignItems: "center" };
+  const imgStyle = { margin: "20px", fontSize: 90 };
+  const paragraphStyle = { margin: "5px" };
 
   return (
     <div style={wrapperStyle} id='about'>
@@ -26,40 +27,57 @@ export default function About() {
 
       <Accordion>
         <Accordion.Item eventKey='0'>
-          <Accordion.Header>Accessibility</Accordion.Header>
+          <Accordion.Header>Why I Value Accesibility</Accordion.Header>
           <Accordion.Body>
             <Card style={{ display: "flex", flexDirection: "row" }}>
-              <Card.Img variant='top' src={deaf} alt='deaf person with cochlear implant' />
+              <FaDeaf style={imgStyle} />
               <Card.Title style={cardStyle}>
-                With a deaf mother, communicating effectively is everything. From this, I pursue
-                exploring code for accessibility, insightful UI/UX, and how I can better bridge
-                information to people.
+                <p style={paragraphStyle}>
+                  With a deaf mother and dyslexic brother, communicating effectively is everything.
+                </p>
+                <p style={paragraphStyle}>
+                  I value accessibility, insightful UI, and strive to better connect info to people.
+                </p>
               </Card.Title>
             </Card>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey='1'>
-          <Accordion.Header>Finance</Accordion.Header>
+          <Accordion.Header>How I First Started Coding</Accordion.Header>
           <Accordion.Body>
             <Card style={{ display: "flex", flexDirection: "row" }}>
+              <HiCursorClick style={imgStyle} />
               <Card.Title style={cardStyle}>
-                Student loan debt is a huge problem. I worked to help solve it for families by
-                programming a college value calculator and one-click template emails. This impact
-                sparked me into coding.
+                <p style={paragraphStyle}>
+                  When I was a finacial advisor, I programmed a college value calculator and
+                  one-click template emails.
+                </p>
+                <p style={paragraphStyle}>This impact sparked me into coding. </p>
               </Card.Title>
-              <Card.Img variant='top' src={banking} alt='person watering plants where ideas grow' />
             </Card>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey='2'>
-          <Accordion.Header>Instructor</Accordion.Header>
+          <Accordion.Header>What I Teach as a Bootcamp Instructor</Accordion.Header>
           <Accordion.Body>
             <Card style={{ display: "flex", flexDirection: "row" }}>
-              <Card.Img variant='top' src={instructor} alt='software engineer coding at a desk' />
+              <FaLaptopCode style={imgStyle} />
               <Card.Title style={cardStyle}>
-                I teach full-stack software engineering with a focus in Javascript, CSS, React,
-                Node, Express, MongoDB, and SQL. I continue to further my skills to build great
-                websites and apps.
+                <p>I teach Javascript, CSS, HTML, React, Node, Express, MongoDB, and SQL.</p>
+              </Card.Title>
+            </Card>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey='3'>
+          <Accordion.Header>Where I'm Headed as a Software Engineer</Accordion.Header>
+          <Accordion.Body>
+            <Card style={{ display: "flex", flexDirection: "row" }}>
+              <FaCodeBranch style={imgStyle} />
+              <Card.Title style={cardStyle}>
+                <p>
+                  While I'm thankful to teach, my goal is to build great products and services that
+                  improve people's quality of life.
+                </p>
               </Card.Title>
             </Card>
           </Accordion.Body>
