4f962d866e5c1393a6d84b65339919781efd31c9
Eric Hoover <ehoover3@yahoo.com>
2022-04-26 21:13:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import ContactMe from "./components/ContactMe";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavigationBar />
+      <Home />
+      <About />
+      <Projects />
+      <ContactMe />
     </div>
   );
 }
-
-export default App;
