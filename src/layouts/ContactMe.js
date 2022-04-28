9d56cb441371df6e2199226bac5ce271947c2abe
Eric Hoover <ehoover3@yahoo.com>
2022-04-28 03:17:16 -0700

    updated contact me sections placeholder color

--- src/layouts/ContactMe.js
+++ src/layouts/ContactMe.js
@@ -4,6 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function ContactMe(props) {
+  const theme = props.theme;
   const handleTheme = props.handleTheme;
 
   const wrapperStyle = {
@@ -12,6 +13,8 @@ export default function ContactMe(props) {
     textAlign: "center",
     marginTop: "30px",
     marginBottom: "100px",
+    marginLeft: "2.5vw",
+    marginRight: "2.5vw",
   };
 
   return (
@@ -21,22 +24,38 @@ export default function ContactMe(props) {
 
       <Form>
         <Form.Group className='mb-3' controlId='formBasicName'>
-          <Form.Control type='name' placeholder='Name' />
+          <Form.Control
+            type='name'
+            placeholder='Name'
+            style={{ backgroundColor: theme.style.primaryColor, color: "white" }}
+          />
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Control type='email' placeholder='Email' />
+          <Form.Control
+            type='email'
+            placeholder='Email'
+            style={{ backgroundColor: theme.style.primaryColor, color: "white" }}
+          />
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='formBasicSubject'>
-          <Form.Control type='subject' placeholder='Subject' />
+          <Form.Control
+            type='subject'
+            placeholder='Subject'
+            style={{ backgroundColor: theme.style.primaryColor, color: "white" }}
+          />
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='formBasicMessage'>
-          <Form.Control type='message' placeholder='Message' />
+          <Form.Control
+            type='message'
+            placeholder='Message'
+            style={{ backgroundColor: theme.style.primaryColor, color: "white" }}
+          />
         </Form.Group>
 
-        <Button variant='primary' type='submit'>
+        <Button variant={theme.style.bootstrap} type='submit'>
           Submit
         </Button>
       </Form>
